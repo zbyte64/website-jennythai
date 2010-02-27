50008c26013a60ac75e0a7d0af6f311625b24886
Jason <zbyte@zbyte-laptop.(none)>
2010-02-26 21:03:27 -0800

    actually test flcikr form

--- jenny/gallery/tests.py
+++ jenny/gallery/tests.py
@@ -1,23 +1,33 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.utils.datastructures import MultiValueDict
+from django.core.files.base import File
+from django.conf import settings
+
+from forms import GalleryForm
+
+import os
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+        from forms import flickr
+        user = flickr.User('43513297@N03')
+        photo_sets = user.getPhotosets()
+        photo = photo_sets[0].getPhotos()[0]
+        #print photo_sets[0].getPhotos()[0].getURL('Original', 'source')
+        data = MultiValueDict({'name': 'test',
+                               'slug': 'test',
+                               'description': 'test',
+                               'order':'1',
+                               'thumbnail': 'thumbnail',
+                               'tall_side_image': 'side_image'})
+        data.setlist('photo_ids', [photo.id])
+        test_photo = os.path.join(settings.PROJECT_DIR, 'jennytphotography_website-scale.jpg')
+        files = {'thumbnail':File(open(test_photo, 'rb')),
+                 'tall_side_image':File(open(test_photo, 'rb'))}
+        form = GalleryForm(data, files)
+        self.assertTrue(form.is_valid(), form.errors)
+        gallery = form.save()
+        self.assertTrue(gallery.photos.all().count())
+        gallery.delete()
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
 
