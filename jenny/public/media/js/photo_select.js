bb2466f0aea28d51d0f4d704f31d9d2f2ece73bc
Jason <zbyte@zbyte-laptop.(none)>
2010-03-01 21:20:00 -0800

    yet another strategy

--- jenny/public/media/js/photo_select.js
+++ jenny/public/media/js/photo_select.js
@@ -1,11 +1,9 @@
 $(document).ready(function(){
-    $('#gallery_form .photo_ids > div').append('<div id="selected_photos">Added Photos:<textinput id="id_photo_ids" "name="photo_ids_text"/></div>');
-    var href = '/gallery/select/?field=photo_ids_text';
-    $('#selected_photos').after('<iframe frameborder="0" style="border:none; width:755px; height:210px;" src="' + href + '"></iframe>');
-    $('#gallery_form').submit(function() {
-        var value = $('#id_photo_ids').val().split();
-        $.each(value, function() {
-            $('#selected_photos').append('<input type="hidden" name="photo_ids" value="'+this.split('/').pop()+'"/>');
-        });
+    $('#gallery_form .photo_ids > div').append('<div id="selected_photos">Added Photos:</div>');
+    $('#selected_photos a.remove').click('live', function() {
+        $(this).remove();
+        return false;
     });
+    var href = '/gallery/select/?field=selected_photos';
+    $('#selected_photos').after('<iframe frameborder="0" style="border:none; width:755px; height:210px;" src="' + href + '"></iframe>');
 });
