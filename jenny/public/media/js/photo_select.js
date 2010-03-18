08a6ef72fd32e7a705023aa164ad3a0d00ca31f2
Jason Kraus <admin@thebadmintonstore.net>
2010-03-18 02:30:14 +0000

    more updates

--- jenny/public/media/js/photo_select.js
+++ jenny/public/media/js/photo_select.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $('#gallery_form .photo_ids > div').append('<div id="selected_photos">Added Photos:</div>');
-    var href = '/gallery/select/?field=selected_photos';
+    var href = '/portfolio/select/?field=selected_photos';
     $('#selected_photos').after('<iframe frameborder="0" style="border:none; width:755px; height:210px;" src="' + href + '"></iframe>');
 });
